1b75a167e38be5e0e6a3135203a233e003e5214f
salviosage <38810299+salviosage@users.noreply.github.com>
2018-07-16 14:13:47 +0200

    about

--- imports/ui/about.jsx
+++ imports/ui/about.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
-function about(){ 
-  
+function about(){
+
     return (
     <div>
-<div> <p>we are on about dfdfhdfhg</p></div>
+<div> <div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>About us</h2>
+      <hr/>
+    </div>
+    <div class="row">
+      <div class="col-md-4"><img src="img/about.png" class="img-responsive"/></div>
+      <div class="col-md-4" >
+        <div class="about-text">
+          <h4>WHO AM I</h4>
+          <p>
+I software developer. In addition to my love of computers  programming,I like to Meet New Friends , Grobal Network, Local Network, IT Security.</p>
+          <p> I have the technical & communication knowledge. I assist people in managing their help desk or work with them to devise a contingency plan for proper computer maintenance and troubleshooting </p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="about-text">
+          <h4>WHAT I DO</h4>
+
+          <p>I am a very skilled in programming and development of the system especially web/android app . and i'm good at data science .</p>
+          <ul>
+            <li>computer programming</li>
+            <li>web/android apps development </li>
+            <li>web designing and development</li>
+            <li>database configuring experience with wamp/XAMP/MAMP and db like pl & sql</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div></div>
      </div>
     );
-  
+
 }
 export default about;
