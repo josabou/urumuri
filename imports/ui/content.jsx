bf6f5b69ce105342139c9d3ca368f92c49963abf
salviosage <38810299+salviosage@users.noreply.github.com>
2018-07-17 13:42:18 +0200

    contents

--- imports/ui/content.jsx
+++ imports/ui/content.jsx
@@ -3,37 +3,105 @@ function content(){
 
     return (
 <div>
-<div className="gotit">
+<div class="container">
 <input type="text" placeholder="Search.."/>
-<div className="dropdown">
-    <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
-    <span className="caret"></span></button>
-    <ul className="dropdown-menu">
+<div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
       <li><a href="#">umubyeyi </a></li>
       <li><a href="#">aratwite</a></li>
       <li><a href="#">umwana</a></li>
     </ul>
   </div>
-  <div className="dropdown">
-      <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
-      <span className="caret"></span></button>
-      <ul className="dropdown-menu">
+  <div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
+      <span class="caret"></span></button>
+      <ul class="dropdown-menu">
         <li><a href="#">umubyeyi </a></li>
         <li><a href="#">aratwite</a></li>
         <li><a href="#">umwana</a></li>
       </ul>
     </div>
-    <div className="dropdown">
-        <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
-        <span className="caret"></span></button>
-        <ul className="dropdown-menu">
+    <div class="dropdown">
+        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">choose category
+        <span class="caret"></span></button>
+        <ul class="dropdown-menu">
           <li><a href="#">umubyeyi </a></li>
           <li><a href="#">aratwite</a></li>
           <li><a href="#">umwana</a></li>
         </ul>
       </div>
       <button type="submit"><i class="fa fa-search"></i></button>
+
 </div>
+<div class="blankSeparator"></div>
+<div class="container">
+  <div class="info">
+    <div class="col-xs-3">
+      <h3>feeding infos</h3>
+      <p>
+
+Nutrition during pregnancy and in the first years of a child’s life provides the essential building blocks for brain development, healthy growth and a strong immune system. In fact, a growing body of scientific evidence shows that the foundations of a person’s lifelong health—including their predisposition to obesity and certain chronic diseases—are largely set during this 1,000 day window.
+
+ </p>
+      <a href="http://themeforest.net/item/positivo-responsive-and-fresh-wp-theme/2700306?ref=anariel7" title="" class="buttonhome">&rarr; download pdf</a> </div>
+    <div class="two_third lastcolumn">
+      <div class="col-xs-4">
+        <h3>parents overview</h3>
+        <p>The right nutrition during this 1,000 day window has a profound impact on a child’s ability to grow, learn and thrive—and a lasting effect on a country’s health and prosperity.</p>
+        <a href="http://themeforest.net/item/positivo-responsive-and-fresh-wp-theme/2700306?ref=anariel7" title="" class="buttonhome">&rarr; download pdf</a> </div>
+      <div class="col-xs-4 lastcolumn">
+        <h3>childs overview</h3>
+        <p>It is why it is critical that women and children get the right nutrition during this time. Malnutrition early in life can cause irreversible damage to children’s brain development and their physical growth, leading to a diminished capacity to learn, poorer performance in school, greater susceptibility to infection and disease and a lifetime of lost earning potential. It can even put them at increased risk of developing illnesses like heart disease, diabetes and certain types of cancers later in life</p>
+        <a href="http://themeforest.net/item/positivo-responsive-and-fresh-wp-theme/2700306?ref=anariel7" title="" class="buttonhome">&rarr; download pdf</a> </div>
+    </div>
+  </div>
+</div>
+
+<div class="container clients">
+  <div class="sepContainer"></div>
+  <h3>Our Clients</h3>
+  <div class="col-xs-2"> <img src="images/client1.jpg" alt=""/> </div>
+
+  <div class="col-xs-2"> <img src="images/client2.jpg" alt=""/> </div>
+
+  <div class="col-xs-2"> <img src="images/client3.jpg" alt=""/> </div>
+
+  <div class="col-xs-2"> <img src="images/client4.jpg" alt=""/> </div>
+
+  <div class="col-xs-2"> <img src="images/client5.jpg" alt=""/> </div>
+
+  <div class="col-xs-2 lastcolumn"> <img src="images/client6.jpg" alt=""/> </div>
+
+</div>
+
+<div class="container boxthree">
+  <div class="sepContainer1"></div>
+  <div class="blankSeparator"></div>
+  <div class="col-xs-3">
+    <section class="boxthreeleft"> <img src="images/home/1.png" alt=""/>
+      <h3>Natural beauty</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <a class ="simple" href="#">+ Learn more</a> </section>
+  </div>
+
+  <div class="col-xs-3">
+    <section class="boxthreecenter"> <img src="images/home/2.png" alt=""/>
+      <h3>Fashion Love</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <a class ="simple" href="#">+ Learn more</a> </section>
+  </div>
+
+  <div class="col-xs-3 lastcolumn">
+    <section class="boxthreeright"> <img src="images/home/3.png" alt=""/>
+      <h3>Modern Design Interior</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <a class ="simple" href="#">+ Learn more</a> </section>
+  </div>
+</div>
+
+<div class="blankSeparator1"></div>
 </div>
     );
 
