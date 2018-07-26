3433817a8759215d605804a45ff26badcc20ea0b
salviosage <38810299+salviosage@users.noreply.github.com>
2018-07-26 11:17:06 +0200

    dashboard lol

--- imports/ui/content.jsx
+++ imports/ui/content.jsx
@@ -128,20 +128,7 @@ render (){
   </div>
 </div>
 <div className="blankSeparator1"></div>
-<button className="accordion">Section 1</button>
-<div className="panel">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-</div>
 
-<button className="accordion">Section 2</button>
-<div className="panel">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-</div>
-
-<button className="accordion">Section 3</button>
-<div className="panel">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-</div>
 <div className="blankSeparator1"></div>
 <div>
 
