4d92d6f99aded06f1e0daa86730c92bb141903ba
Josiane <josabou@yahoo.com>
2018-08-01 00:53:42 +0200

    knfkfndk

--- server/main.js
+++ server/main.js
@@ -2,7 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/ourdb/Users.js';
 import { user } from '../imports/api/user.js'
 import { child } from '../imports/api/child.js'
-import { user } from '../imports/api/user.js'
 import '../imports/ourdb/Contents.js';
 //import '../lib/at_config.js'
 
