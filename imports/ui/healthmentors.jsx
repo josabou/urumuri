7fa5aea18278c9ac67b69b5e5f79bca33d250041
Josiane <josabou@yahoo.com>
2018-08-02 13:51:24 +0200

    made some changes

--- imports/ui/healthmentors.jsx
+++ imports/ui/healthmentors.jsx
@@ -186,8 +186,8 @@ class healthmentors extends React.Component {
                         <a href="#" className="w3-bar-item w3-button" onClick={this.showOverview.bind(this)}>OVERVIEW</a>
                         <a href="#" className="w3-bar-item w3-button" onClick={this.showMessage.bind(this)}>MESSAGE</a>
                     </div>
-</div>
-                    <div style={{alignContent:"center"}}>
+</div><center>
+                    <div>
                         <button style={{ width: "100px" }} className="w3-button w3-white w3-xxlarge" onClick={this.w3_open.bind(this)}>&#9776;</button>
 
                         <div className="center-block" style={{ margin: "0px 300px 0px 300px" }} >
@@ -687,6 +687,7 @@ class healthmentors extends React.Component {
                             </div>
                         </div>
                     </div>
+                    </center>
 
 
             </div>
